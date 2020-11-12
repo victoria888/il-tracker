13c117267e1bf1d4e64cee1c469dd4ea91a3f0d4
chidinma.egwuatu <chidinma.egwuatu@cicod.com>
2020-11-12 17:05:46 +0100

    implemented track page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TrackerComponent } from './tracker/tracker.component';
+import { HomeComponent } from './utils/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'tracker',
+    component: TrackerComponent,
+  },
+  {
+    path:'',
+    component: HomeComponent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrackerComponent } from './tracker/tracker.component';
+import { HeaderComponent } from './utils/header/header.component';
+import { FooterComponent } from './utils/footer/footer.component';
+import { HomeComponent } from './utils/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrackerComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
